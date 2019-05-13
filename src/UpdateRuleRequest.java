69f5b614a6a785ef9db3ff92ece941b5007e490e
btg99 <btg99email@gmail.com>
2019-05-13 16:36:56 -0400

    Made dummy UpdateRuleRequest to the UpdateRuleInteractor

--- src/UpdateRuleRequest.java
+++ src/UpdateRuleRequest.java
@@ -1,1 +1,9 @@
-public class UpdateRuleRequest {}
+public class UpdateRuleRequest {
+  public UpdateRuleRequest(
+        boolean requiresActive, boolean requiresOneYearCooldown, int yearsPerTerm,
+        int consecutiveTermsBeforeCooldown, Rule.TenureRequirements tenureRequirement,
+        Rule.Divisions division)
+  {
+
+  }
+}
--- test/UpdateRuleInteractorTest.java
+++ test/UpdateRuleInteractorTest.java
@@ -2,8 +2,18 @@ import org.junit.Test;
 
 public class UpdateRuleInteractorTest {
   @Test
-  public void canExecuteInteractor(){
+  public void canExecuteInteractorWithDummyRequest(){
     UpdateRuleInteractor interactor = new UpdateRuleInteractor();
-    interactor.execute(new UpdateRuleRequest());
+
+    boolean requiresActive = false;
+    boolean requiresOneYearCooldown = false;
+    int yearsPerTerm = 0;
+    int consecutiveTermsBeforeCooldown = 0;
+    Rule.TenureRequirements tenureRequirement = Rule.TenureRequirements.None;
+    Rule.Divisions division = Rule.Divisions.AtLarge;
+
+    interactor.execute(new UpdateRuleRequest(requiresActive, requiresOneYearCooldown,
+                                             yearsPerTerm, consecutiveTermsBeforeCooldown,
+                                             tenureRequirement, division));
   }
 }
