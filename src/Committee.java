1d634f47d2dc2575a6cdef4f528dbdebfd7b7487
Trixia Yuzon <yuzont19@hanover.edu>
2019-05-13 14:03:48 -0400

    Create Committee test and class files

---
+++ src/Committee.java
@@ -0,0 +1,20 @@
+public class Committee {
+  private String name;
+  private String description;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
