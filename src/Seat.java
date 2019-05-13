3114b354ae3c735570e8a8dc8045f0489296cc66
Trixia Yuzon <yuzont19@hanover.edu>
2019-05-13 16:26:53 -0400

    Add Profile to the seat and add Rule to the seat

--- src/Seat.java
+++ src/Seat.java
@@ -1,2 +1,21 @@
 public class Seat {
+  private String profile;
+  private Rule rule;
+
+  public void setProfile(String name) {
+    this.profile = name;
+
+  }
+
+  public String getProfile() {
+    return profile;
+  }
+
+  public void setRule(Rule rule) {
+    this.rule = rule;
+  }
+
+  public Rule getRule() {
+    return rule;
+  }
 }
