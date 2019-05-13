e07f217a2912952451b8a9ce18e8e5627afaeba4
ganzorigo21 <od.ganzorig12@gmail.com>
2019-05-13 16:01:12 -0400

    Fixed profile error

---
+++ test/BallotCreatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BallotCreatorTest {
+
+  @Test
+
+  public void newBallotIsCreated(){
+    BallotCreator ballotCreator = new BallotCreator();
+  }
+
+
+}
