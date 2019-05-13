7340e7b8473f5cfa59fbb9533ff5ef5ce0d60ac1
ganzorigo21 <od.ganzorig12@gmail.com>
2019-05-13 14:48:36 -0400

    second query test

--- test/QueryTest.java
+++ test/QueryTest.java
@@ -1,18 +1,21 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class QueryTest {
 
   @Test
   public void createQuery(){
     Query query = new Query();
   }
-//
-//  @Test
-//  public void ProfileIsAlwaysFalse{
-//    Query query = new Query();
-//    Profile profile = new Profile(Todd, Art);
-//    assertEquals(false, query.isProfileValid(profile));
-//
-//  }
+
+  @Test
+  public void ProfileIsAlwaysFalse(){
+    Query query = new Query();
+    Profile profile = new Profile("Todd", "Art");
+    assertFalse(query.isProfileValid(profile));
+
+  }
 
 }
