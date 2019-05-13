c81cfdba936b64216b86466815e1439041233581
ganzorigo21 <od.ganzorig12@gmail.com>
2019-05-13 16:55:45 -0400

    Test ballotCreator on AlwaysTrueQuery

--- src/BallotCreator.java
+++ src/BallotCreator.java
@@ -1,6 +1,22 @@
 public class BallotCreator {
 
-  public Ballot getBallot(AlwaysFalseQuery alwaysFalseQuery) {
-    return new Ballot();
+  private Gateway gateway;
+
+  public Ballot getBallot(Query query) {
+    Ballot ballot = new Ballot();
+    addValidProfilesToBallot(query, ballot);
+    return ballot;
+  }
+
+  private void addValidProfilesToBallot(Query query, Ballot ballot) {
+    for (Profile profile : gateway.getAllProfiles()) {
+      if (query.isProfileValid(profile)) {
+        ballot.add(profile);
+      }
+    }
+  }
+
+  public void setGateway(Gateway gateway) {
+    this.gateway = gateway;
   }
 }
