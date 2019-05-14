f0dc5e06536a352005248902b4cb5813b03b0e8f
caldwellp <caldwellp20@hanover.edu>
2019-05-14 14:22:58 -0400

    Added EditBallotInteractor

---
+++ src/fsc/interactor/EditBallotInteractor.java
@@ -0,0 +1,12 @@
+package fsc.interactor;
+
+import fsc.entity.Profile;
+
+import java.util.AbstractList;
+
+public class EditBallotInteractor {
+  public static AbstractList<Profile> execute(Profile profile) {
+
+    return null;
+  }
+}
--- test/fsc/interactor/EditBallotInteractorTest.java
+++ test/fsc/interactor/EditBallotInteractorTest.java
@@ -3,17 +3,27 @@ package fsc.interactor;
 import fsc.entity.Ballot;
 import fsc.entity.Profile;
 import fsc.request.AddToBallotRequest;
+import org.junit.Before;
 import org.junit.Test;
 
-public class AddToBallotRequestTest {
+import java.util.AbstractList;
+import java.util.ArrayList;
 
-  @Test
+public class EditBallotInteractorTest {
+
+  private Profile profile;
+  private AbstractList<Profile> profiles;
+
+  @Before
   public void makeAddToBallotRequest() {
     Profile profile = new Profile("name1", "username1", "department1", "contract1");
     Ballot ballot = new Ballot();
     AddToBallotRequest request = new AddToBallotRequest(ballot.getID(), profile);
   }
 
-
+  @Test
+  public void testExecute() {
+    profiles = EditBallotInteractor.execute(profile);
+  }
 
 }
