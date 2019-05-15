f400dc814a753208f5328765da204837aead7315
courtneya <arrowoodc22@hanover.edu>
2019-05-15 14:35:24 -0400

    Cleaned up and Refactored ProfileViewerInteractor and its Test

--- src/fsc/interactor/ProfileReviewInteractor.java
+++ src/fsc/interactor/ProfileReviewInteractor.java
@@ -4,11 +4,8 @@ import fsc.entity.Profile;
 import fsc.gateway.ProfileGatewayInterface;
 import fsc.response.Response;
 
-import java.util.HashMap;
-
 public class ProfileReviewInteractor {
   public String userName;
-  public HashMap<String, String> userInfo = null;
   public ProfileGatewayInterface gateway;
 
   public ProfileReviewInteractor(ProfileGatewayInterface gateway){
@@ -18,21 +15,17 @@ public class ProfileReviewInteractor {
   public Response execute(ProfileViewerRequest request) {
     userName = request.userName;
     try{
-      Profile profile = gateway.getProfile(userName);
-      ProfileViewer profileInfo = new ProfileViewer(profile);
-      userInfo = profileInfo.createHashMap();
-      ProfileViewerResponse response = new ProfileViewerResponse(userInfo);
-      return response;
+      return tryCreateProfileResponse();
     }
     catch (Exception e){
       return new ErrorResponse("No profile found!");
     }
   }
-  public class NoProfileException extends RuntimeException {
-    public String noProfile;
-    public NoProfileException(String s) {
-      noProfile = s;
-    }
+
+  private Response tryCreateProfileResponse() {
+    Profile profile = gateway.getProfile(userName);
+    ProfileViewer profileInfo = new ProfileViewer(profile);
+    return new ProfileViewerResponse(profileInfo.createHashMap());
   }
 
   public class ErrorResponse implements Response {
--- test/fsc/interactor/ProfileViewerInteractorTest.java
+++ test/fsc/interactor/ProfileViewerInteractorTest.java
@@ -1,17 +1,13 @@
 package fsc.interactor;
 
 import fsc.response.Response;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertTrue;
 
 public class ProfileViewerInteractorTest {
-  // This method creates a Hash Map
-  // Has a request method that turns it into a response.
   @Test
-  public void RequestErrorTest_NoUsername() throws ProfileReviewInteractor.NoProfileException {
+  public void RequestErrorTest_NoUsername() {
     ProfileViewerRequest request = new ProfileViewerRequest("BoogieA14");
     noProfileGateWaySpy gateway = new noProfileGateWaySpy();
     ProfileReviewInteractor viewInteractor = new ProfileReviewInteractor(gateway);
