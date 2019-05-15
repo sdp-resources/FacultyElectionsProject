03e74d56bf8fac8b802d89c1cce774bb919b1cd5
caldwellp <caldwellp20@hanover.edu>
2019-05-15 15:11:11 -0400

    ballot does not need to generate ID and still working addToBallotInteractor

---
+++ src/fsc/interactor/AddToBallotGatewayInterface.java
@@ -0,0 +1,8 @@
+package fsc.interactor;
+
+import fsc.entity.Ballot;
+
+public interface AddToBallotGatewayInterface {
+
+  Ballot getBallot(String id);
+}
--- src/fsc/interactor/AddToBallotInteractor.java
+++ src/fsc/interactor/AddToBallotInteractor.java
@@ -1,19 +1,27 @@
 package fsc.interactor;
 
+import fsc.entity.Ballot;
 import fsc.gateway.Gateway;
 import fsc.request.AddToBallotRequest;
 import fsc.response.AddToBallotResponse;
 
 public class AddToBallotInteractor {
-      public AddToBallotInteractor(Gateway gateway) {
+  public AddToBallotGatewayInterface gateway;
 
-      }
+      public AddToBallotInteractor(AddToBallotGatewayInterface gateway) { this.gateway = gateway; }
 
-      public static AddToBallotResponse execute(AddToBallotRequest request) {
+      public AddToBallotResponse execute(AddToBallotRequest request) throws NoBallotException {
         String ballotID = request.getBallotID();
-        if (ballotID == null) {
+        /*try{
+          Ballot ballot = gateway.getBallot(ballotID);
 
-        }
+        }*/
+
         return null;
       }
+
+
+  private class NoBallotException extends Exception {
+
+  }
 }
