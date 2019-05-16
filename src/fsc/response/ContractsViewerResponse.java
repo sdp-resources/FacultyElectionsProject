c0042e91031aae7ef28b093689e32f62b38196ac
jstricker21 <Hanover#18>
2019-05-16 14:50:58 -0400

    Wrote test for ContractViewerInteractor and added ContractViewerGateway interface with implementation.

--- src/fsc/response/ContractsViewerResponse.java
+++ src/fsc/response/ContractsViewerResponse.java
@@ -1,10 +1,21 @@
 package fsc.response;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ContractsViewerResponse implements Response{
   List<String> contracts;
+
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContractsViewerResponse that = (ContractsViewerResponse) o;
+    return Objects.equals(contracts, that.contracts);
+  }
+
+  public int hashCode() {
+    return Objects.hash(contracts);
+  }
 
   public ContractsViewerResponse(List<String> contracts){
     this.contracts = contracts;
