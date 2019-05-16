d74f612267ad9bb043284fb3ba6b058c9d53f889
Jonathan Helmers <helmersj20@hanover.edu>
2019-05-16 16:23:54 -0400

    clean up code in the CreateContractTypeInteractorTest

--- test/fsc/interactor/CreateContractTypeInteractorTest.java
+++ test/fsc/interactor/CreateContractTypeInteractorTest.java
@@ -5,19 +5,18 @@ import fsc.request.CreateContractTypeRequest;
 import fsc.response.Response;
 import org.junit.Before;
 import org.junit.Test;
-
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class CreateContractTypeInteractorTest {
 
+  public static final String CONTRACT_TYPE = "tenured";
   CreateContractTypeRequest request;
   CreateContractTypeInteractor interactor;
   Response response;
 
   @Before
   public void setup() {
-    request =  new CreateContractTypeRequest("tenured");
+    request =  new CreateContractTypeRequest(CONTRACT_TYPE);
   }
 
   @Test
@@ -25,8 +24,7 @@ public class CreateContractTypeInteractorTest {
     NoSuchContractTypeContractTypeGatewaySpy gateway = new NoSuchContractTypeContractTypeGatewaySpy();
     interactor = new CreateContractTypeInteractor(gateway);
     response = interactor.execute(request);
-    assertEquals("tenured", gateway.submittedContractType);
-    //assertTrue(response instanceof SuccessfullyAddedProfileResponse);
+    assertEquals(CONTRACT_TYPE, gateway.submittedContractType);
   }
 
 }
