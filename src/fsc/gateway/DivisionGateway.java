948b7f3ed313231b98c01789e7a00ac346b080e3
Haris Skiadas <cskiadas@gmail.com>
2019-05-22 12:49:45 -0400

    Fix typo

--- src/fsc/gateway/DivisionGateway.java
+++ src/fsc/gateway/DivisionGateway.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface DivisionGateway {
   List<String> getAllDivisions();
   String getDivision(String divisionName) throws Exception;
-  void addDivision(String divsion);
+  void addDivision(String division);
   void save();
   Boolean hasDivision(String divisionName);
 }
