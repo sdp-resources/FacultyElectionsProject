e1081a399e761c2598b5a9ed486d6d8a8c0aa495
Haris Skiadas <cskiadas@gmail.com>
2019-05-30 23:56:56 -0400

    Refactor the string-to-query functions

--- src/fsc/service/QueryStringConverter.java
+++ src/fsc/service/QueryStringConverter.java
@@ -5,6 +5,7 @@ import fsc.entity.query.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class QueryStringConverter implements Query.QueryVisitor {
 
@@ -35,22 +36,21 @@ public class QueryStringConverter implements Query.QueryVisitor {
   }
 
   public Query fromString(String s) {
-    return new StringParser(s).parse();
+    return new QueryStringParser(s).parse();
   }
 
   public String toString(Query query) {
     return (String) visit(query);
   }
 
-  private class StringParser {
+  private class QueryStringParser {
     private Scanner scanner;
 
-    public StringParser(String s) {
+    public QueryStringParser(String s) {
       scanner = new Scanner(s);
     }
 
     public Query parse() {
-      System.out.println();
       Query query = processNext();
       if (scanner.hasNext()) {
         throw new InvalidStringException();
@@ -59,55 +59,88 @@ public class QueryStringConverter implements Query.QueryVisitor {
     }
 
     private Query processNext() {
-      if (tryReadWord("all")) {
-        return Query.always();
-      } else if (tryReadWord("none")) {
-        return Query.never();
-      } else if (tryReadParens("\\(")) {
-        return processParentheses();
-      } else {
-        return processSimpleCase();
-      }
-    }
-
-    private boolean tryReadWord(String word) {
-      scanner.useDelimiter("AND|OR|\\)|\\s+");
-      boolean check = scanner.hasNext(word);
-      if (check) scanner.next(word);
-      return check;
-    }
-
-    private boolean tryReadParens(String pattern) {
-      scanner.useDelimiter("");
-      while (scanner.hasNext("\\s")) { scanner.next("\\s"); }
-      boolean check = scanner.hasNext(pattern);
-      if (check) scanner.next(pattern);
-      scanner.reset();
-      return check;
+      if (tryReadWord("all")) return Query.always();
+      if (tryReadWord("none")) return Query.never();
+      if (tryReadOpenParens()) return processParentheses();
+      return processSimpleCase();
     }
 
     private Query processSimpleCase() {
-      String key =  scanner.next();
+      String key = readNext();
       scanner.next("equals");
-      scanner.useDelimiter("AND|OR|\\)");
-      String value = scanner.next().trim();
-      scanner.reset();
+      String value = readPhrase();
       return Query.has(key, value);
     }
 
     private Query processParentheses() {
       List<Query> queries = new ArrayList<>();
-      queries.add(processNext());
-      if (tryReadParens("\\)")) {
-        return queries.get(0);  // case of single
+      return processAndPopulateList(queries);
+    }
+
+    private Query processAndPopulateList(List<Query> queries) {
+      String connective = null;
+      addNextQuery(queries);
+      while (!tryReadCloseParens()) {
+        connective = scanPastConnective(connective);
+        addNextQuery(queries);
       }
-      String connective = scanner.next("AND|OR").trim();
-      queries.add(processNext());
-      while(!tryReadParens("\\)")) {
-        scanner.next(connective);
-        queries.add(processNext());
-      }
-      return connective.equals("AND") ? Query.all(queries) : Query.any(queries);
+      return queries.size() == 0 ? queries.get(0)
+                                 : connective.equals("AND")
+                                   ? Query.all(queries)
+                                   : Query.any(queries);
+    }
+
+    private String scanPastConnective(String connective) {
+      return connective == null ? scanner.next("AND|OR").trim()
+                                : scanner.next(connective);
+    }
+
+    private boolean addNextQuery(List<Query> queries) {
+      return queries.add(processNext());
+    }
+
+    private boolean tryReadWord(String word) {
+      return tryReadWithDelimiter(word, "AND|OR|\\)|\\s+");
+    }
+
+    private boolean tryReadOpenParens() {
+      return tryReadWithDelimiter("\\(", "");
+    }
+
+    private boolean tryReadCloseParens() {
+      return tryReadWithDelimiter("\\)", "");
+    }
+
+    private boolean tryReadWithDelimiter(String pattern, String delimiter) {
+      return doWithDelimiter(delimiter, () -> {
+        skipWhitespace();
+        return checkAndSkip(pattern);
+      });
+    }
+
+    private void skipWhitespace() {
+      while (scanner.hasNext("\\s")) { scanner.next("\\s"); }
+    }
+
+    private Boolean checkAndSkip(String pattern) {
+      boolean check = scanner.hasNext(pattern);
+      if (check) scanner.next(pattern);
+      return check;
+    }
+
+    private String readPhrase() {
+      return doWithDelimiter("AND|OR|\\)", () -> readNext().trim());
+    }
+
+    private String readNext() {
+      return scanner.next();
+    }
+
+    private <T> T doWithDelimiter(String delimiter, Supplier<T> supplier) {
+      scanner.useDelimiter(delimiter);
+      T value = supplier.get();
+      scanner.reset();
+      return value;
     }
   }
 
