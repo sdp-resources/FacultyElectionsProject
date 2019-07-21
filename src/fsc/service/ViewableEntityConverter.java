50b69295ab15975a0dba024bdebd502ca510d1ac
Haris Skiadas <cskiadas@gmail.com>
2019-07-21 14:57:58 -0400

    Minor cleanup

--- src/fsc/service/ViewableEntityConverter.java
+++ src/fsc/service/ViewableEntityConverter.java
@@ -8,12 +8,21 @@ import fsc.viewable.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ViewableEntityConverter {
 
   private QueryStringConverter queryStringConverter = new QueryStringConverter();
   private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+  private String convert(Division division) {
+    return division.getName();
+  }
+
+  private String convert(ContractType contractType) {
+    return contractType.getContract();
+  }
 
   public ViewableProfile convert(Profile profile) {
     return new ViewableProfile(profile.getName(), profile.getUsername(), profile.getDivision(),
@@ -29,17 +38,23 @@ public class ViewableEntityConverter {
   private ViewableSeat convert(Seat seat) {
     return new ViewableSeat(Long.toString(seat.getId()),
                             seat.getName(),
-                            new QueryStringConverter().toString(seat.getCandidateQuery()),
+                            convert(seat.getCandidateQuery()),
                             nullOrConvert(seat.getProfile()));
   }
 
   public ViewableCandidate convert(Candidate candidate) {
     return new ViewableCandidate(convert(candidate.getProfile()), candidate.getStatus());
-
   }
 
   private String convert(LocalDateTime date) {
     return date.format(dateTimeFormatter);
+  }
+
+  public ViewableVoter convert(Voter voter) {
+    return new ViewableVoter(voter.getVoterId(),
+                             voter.hasVoted(),
+                             convert(voter.getProfile()),
+                             convert(voter.getElection()));
   }
 
   public ViewableVoteRecord convert(VoteRecord voteRecord) {
@@ -59,23 +74,31 @@ public class ViewableEntityConverter {
                                 election.getState().toString(),
                                 convert(election.getSeat()),
                                 convert(election.getCandidateQuery()),
-                                convertBallot(election.getCandidates()));
+                                convertCandidates(election.getCandidates()));
+  }
+
+  private <T, S> List<T> convertList(Collection<S> items, Function<S, T> f) {
+    return items.stream().map(f).collect(Collectors.toList());
+  }
+
+  private Collection<ViewableVoter> convertVoters(Collection<Voter> voters) {
+    return convertList(voters, this::convert);
   }
 
   public Collection<ViewableProfile> convertProfiles(Collection<Profile> profiles) {
-    return profiles.stream().map(this::convert).collect(Collectors.toList());
+    return convertList(profiles, this::convert);
   }
 
   private List<ViewableSeat> convertSeats(Set<Seat> seats) {
-    return seats.stream().map(this::convert).collect(Collectors.toList());
+    return convertList(seats, this::convert);
   }
 
   public List<ViewableCommittee> convertCommittees(List<Committee> committees) {
-    return committees.stream().map(this::convert).collect(Collectors.toList());
+    return convertList(committees, this::convert);
   }
 
   public List<ViewableVoteRecord> convertVoteRecordList(Collection<VoteRecord> voteRecordList) {
-    return voteRecordList.stream().map(this::convert).collect(Collectors.toList());
+    return convertList(voteRecordList, this::convert);
   }
 
   public Map<String, String> convertQueries(Map<String, Query> queries) {
@@ -85,29 +108,22 @@ public class ViewableEntityConverter {
   }
 
   public List<String> convertDivisions(List<Division> divisions) {
-    return divisions.stream().map(Division::getName).collect(Collectors.toList());
+    return convertList(divisions, this::convert);
   }
 
   public List<String> convertContractTypes(List<ContractType> contractTypes) {
-    return contractTypes.stream().map(ContractType::getContract).collect(Collectors.toList());
+    return convertList(contractTypes, this::convert);
   }
 
-  private List<ViewableCandidate> convertBallot(Collection<Candidate> ballot) {
-    return ballot.stream().map(this::convert).collect(Collectors.toList());
+  private List<ViewableCandidate> convertCandidates(Collection<Candidate> candidates) {
+    return convertList(candidates, this::convert);
   }
 
   private ViewableProfile nullOrConvert(Profile profile) {
     return profile == null ? null : convert(profile);
   }
 
-  public ViewableVoter convert(Voter voter) {
-    return new ViewableVoter(voter.getVoterId(),
-                             voter.hasVoted(),
-                             convert(voter.getProfile()),
-                             convert(voter.getElection()));
-  }
-
   public List<String> getUsernames(List<Profile> profiles) {
-    return profiles.stream().map(Profile::getUsername).collect(Collectors.toList());
+    return convertList(profiles, Profile::getUsername);
   }
 }
